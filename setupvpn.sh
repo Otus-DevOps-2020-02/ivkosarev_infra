432910b14e26b50381919d6500e082467e19bec8
ivkosarev <60182235+ivkosarev@users.noreply.github.com>
2020-04-14 17:01:45 +0300

    Update setupvpn.sh

--- setupvpn.sh
+++ setupvpn.sh
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-cat <<EOF> setupvpn.sh
-=======
->>>>>>> cloud-bastion
 #!/bin/bash
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
 echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
@@ -12,8 +8,3 @@ apt-get --assume-yes upgrade
 apt-get --assume-yes install pritunl mongodb-org
 systemctl start pritunl mongod
 systemctl enable pritunl mongod
-<<<<<<< HEAD
-EOF
-=======
-EOF
->>>>>>> cloud-bastion
