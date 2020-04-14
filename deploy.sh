0502e624b33cbe471a60642f1ba36eb24f03dbe6
ivkosarev <60182235+ivkosarev@users.noreply.github.com>
2020-04-14 21:05:59 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#/bin/bash
 
-git clone -b monolith https://github.com/express42/reddit.git 
+cd ~
+git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
