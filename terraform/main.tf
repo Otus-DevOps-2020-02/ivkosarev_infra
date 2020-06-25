319f795e5f50399d88830e58035e9f4580a8cf6e
ivan <ivkosarev@gmail.com>
2020-06-25 23:18:27 +0300

    начинаем пробовать модули

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,9 +34,11 @@ resource "google_compute_instance" "app" {
   }
 
   network_interface {
-    network = "default"
-    access_config {}
-  }
+ network = "default"
+ access_config {
+   nat_ip = google_compute_address.app_ip.address
+ }
+}
   metadata = {
     # путь до публичного ключа
     ssh-keys = "appuser:${file(var.public_key_path)}"
@@ -74,3 +76,20 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name = "default-allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
+
