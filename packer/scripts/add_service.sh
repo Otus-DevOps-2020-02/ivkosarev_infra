86205150a869bf399d1f313fb67e120c5e981f8a
ivan <ivkosarev@gmail.com>
2020-07-10 17:18:18 +0300

    пробуем переложить пакер

--- packer/scripts/add_service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cp /tmp/application.service /etc/systemd/system/application.service
-#systemctl daemon-reload
-chmod 644 /etc/systemd/system/application.service
-systemctl start application.service
-systemctl status application.service
-systemctl enable application.service
