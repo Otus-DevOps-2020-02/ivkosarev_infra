90949a40c407f21573eef4c91ad67b96999fd5bb
ivan <ivkosarev@gmail.com>
2020-07-10 14:55:04 +0300

    ITS ALIVE

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -10,26 +10,27 @@ variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "europe-west1-b"
+}
 variable disk_image {
   description = "Disk image"
 }
-variable "private_key_path" {
-  # Описание переменной
-  description = "Path to the private key used for connection provisioner"
-}
-variable zone {
-  description = "Zone for VM"
-  default     = "europe-west1-d"
-}
-variable ncount {
-  description = "Count for VM"
-  default     = "1"
-}
 variable app_disk_image {
   description = "Disk image for reddit app"
-  default     = "reddit-base"
+  default = "reddit-base"
 }
 variable db_disk_image {
   description = "Disk image for reddit db"
-  default     = "reddit-base"
+  default = "reddit-base"
+}
+variable source_ranges {
+  description = "Allowed IP addresses"
+  default = ["0.0.0.0/0"]
 }
