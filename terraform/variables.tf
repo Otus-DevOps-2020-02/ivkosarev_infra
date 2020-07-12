ed93d4df42d41e3536a1886f4f6bf746b08ac0fe
ivan <ivkosarev@gmail.com>
2020-07-12 17:52:14 +0300

    Разбивка на 2 ВМ скушала мне моцк,памагити \(о_О/

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,5 +19,13 @@ variable "private_key_path" {
 }
 variable zone {
   description = "Zone for VM"
-  default     = "europe-west1-d"
+  default     = "europe-west1-b"
+}
+variable app_disk_image {
+ description = "Disk image for reddit app"
+ default = "reddit-app-base"
+}
+variable db_disk_image {
+ description = "Disk image for reddit db"
+ default = "reddit-db-base"
 }
