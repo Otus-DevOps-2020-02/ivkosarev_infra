bf24689f16ff71ee9b4b3c77ff93e4f230c363b4
ivan <ivkosarev@gmail.com>
2020-07-12 15:29:50 +0300

    Все подготовили для перекладки VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,10 @@ resource "google_compute_instance" "app" {
 
   network_interface {
     network = "default"
-    access_config {}
+    access_config {
+      # Ссылаемся на адрес ВМ
+      nat_ip = google_compute_address.app_ip.address
+    }
   }
   metadata = {
     # путь до публичного ключа
@@ -73,4 +76,22 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
+}
+
+# правило SSH теперь в терраформе!
+resource "google_compute_firewall" "firewall_ssh" {
+  name = "default-allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# Зададим IP для инстанса с приложением в виде внешнего ресурса
+resource "google_compute_address" "app_ip" {
+name = "reddit-app-ip"
 }
