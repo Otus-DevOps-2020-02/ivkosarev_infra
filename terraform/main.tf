b1315037271db2391905e886686a0dd3f8dbdc12
ivan <ivkosarev@gmail.com>
2020-09-14 18:11:29 +0300

    +

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,54 @@
 terraform {
   # Версия terraform
-  required_version = "~>0.12.19"
+  required_version = ">= 0.12.0"
 }
 
 provider "google" {
   # Версия провайдера
-  version = "2.15"
+  version = ">= 2.15"
 
   # ID проекта
-  project = "infra-271212"
+  project = var.project
 
-  region = "europe-west-1"
+  region = var.region
+}
+
+resource "google_compute_project_metadata" "ssh-keys" {
+  metadata = {
+    ssh-keys = "appuser:${file(var.public_key_path)}appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+  }
+  project = var.project
 }
 
 resource "google_compute_instance" "app" {
-  name         = "reddit-app"
+  count        = var.ncount
+  name         = "reddit-app-${count.index}"
   machine_type = "g1-small"
-  zone         = "europe-west1-b"
-
-  # определение загрузочного диска
+  zone         = var.zone
+  tags         = ["reddit-app"]
   boot_disk {
     initialize_params {
-      image = "reddit-base"
+      image = var.disk_image
     }
-  }
-  metadata = {
-    ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
+
   }
 
-  tags = ["reddit-app"]
-
-  # определение сетевого интерфейса
   network_interface {
-    # сеть, к которой присоединить данный интерфейс
     network = "default"
-
-    # использовать ephemeral IP для доступа из Интернет
     access_config {}
   }
-
-  connection {
-     host  = self.network_interface[0].access_config[0].nat_ip
-    type        = "ssh"
-    user        = "appuser"
-    agent       = false
-    private_key = "${file("~/.ssh/appuser")}"
+  metadata = {
+    # путь до публичного ключа
+    ssh-keys = "appuser:${file(var.public_key_path)}"
   }
-
+  connection {
+    type  = "ssh"
+    host  = self.network_interface[0].access_config[0].nat_ip
+    user  = "appuser"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
   provisioner "file" {
     source      = "files/puma.service"
     destination = "/tmp/puma.service"
@@ -55,6 +57,7 @@ resource "google_compute_instance" "app" {
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
+
 }
 
 resource "google_compute_firewall" "firewall_puma" {
